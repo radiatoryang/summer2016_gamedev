b8ec01855090ac7991c837a2b44cc2a62b8fd7a1
Robert Yang <yang.robert.w@gmail.com>
2016-07-12 16:30:04 -0400

    added week02a: intro to code... HelloWorld.cs, TapGame.cs

---
+++ week02/Assets/scripts/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI; // this line will let your code talk to the Text
+
+public class HelloWorld : MonoBehaviour {
+
+	// declare a Text variable so that this code can talk to it
+	public Text myTextObject;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello World");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Bonjour Monde");
+
+		myTextObject.text = "Hola Mundo";
+	}
+}
---
+++ week02/Assets/scripts/TapGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class TapGame : MonoBehaviour {
+
+	public Text myTextThing;
+
+	int currentPoints = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		// give player 1 point if they press Space
+		if ( Input.GetKeyDown(KeyCode.Space) ) {
+			currentPoints += 1;
+			myTextThing.text = "CURRENT SCORE: " + currentPoints.ToString();
+		}
+
+	}
+}
